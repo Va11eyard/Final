c2c9ded4eb326c9d905538900f133cf9201bbe7a
Va11eyard <mynzhasardimash@gmail.com>
2025-02-26 15:24:20 +0500

    остальные. все ще нет скринов зада

---
+++ [9] copy/9.py
@@ -0,0 +1,5 @@
+#9
+A Integration Testing (since it involves component interaction).
+B Stress Testing (since it evaluates system stability under high loads).
+class TestOrder	Integration Testing
+def test_order_under_repeated_processing	Stress Testing / Stability Testing
